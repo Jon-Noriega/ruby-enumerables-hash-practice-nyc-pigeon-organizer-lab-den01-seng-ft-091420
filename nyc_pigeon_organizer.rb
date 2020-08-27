61893169877f6017859351446e118df49b17db3c
Jon Noriega <noriega331@gmail.com>
2020-08-27 15:47:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -14,8 +14,8 @@ final_results = data.each_with_object({}) do |(key, value), final_array|
           !final_array[name][key] = []
         end
             final_array[name][key] << inner_key.to_s
-end
-end
-end
+            end
+          end
+        end
 # binding.pry
 end
