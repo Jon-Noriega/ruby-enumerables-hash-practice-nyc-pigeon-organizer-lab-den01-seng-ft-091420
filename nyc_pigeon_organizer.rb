4be5e430765fe3cc5418de3ea800227f3e526a29
Jon Noriega <noriega331@gmail.com>
2020-08-27 15:19:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,8 @@ require "pry"
 def nyc_pigeon_organizer(data)
 
 data.each_with_object({}) do |(key, value), final_array|
-  binding.pry
-  final_array[key] = value + 3
+  # binding.pry
+  
 final_array
 end
   
